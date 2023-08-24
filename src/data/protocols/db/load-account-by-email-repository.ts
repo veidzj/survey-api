03fa3baf7e6b5e1ce4610d98b3fde2ec1a08f4d7
veidzj <veidzjv@gmail.com>
2023-08-24 09:44:37 -0300

    fix: imports path

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from '../usecases/add-account/db-add-account-protocols'
+import { AccountModel } from '../../../domain/models/account'
 
 export interface LoadAccountByEmailRepository {
   load (email: string): Promise<AccountModel>
