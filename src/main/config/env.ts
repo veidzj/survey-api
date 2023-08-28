4638b97527553f7fce31eb979c70f0a33b6dd7b3
veidzj <veidzjv@gmail.com>
2023-08-27 21:16:11 -0300

    chore: change dev script to use docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/survey-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongodb:27017/survey-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '2+a(4fe1b5=b5b33e-75!3,5c3215cb'
 }
