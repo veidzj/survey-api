255a0fff0338efe2c92aa5165b7d05eed9b68cbd
veidzj <veidzjv@gmail.com>
2023-08-31 18:22:36 -0300

    test: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,41 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+import env from '../config/env'
+
+let surveysCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveysCollection = await MongoHelper.getCollection('surveys')
+    await surveysCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'https://image-url.com'
+            }, {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
