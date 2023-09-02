9161631660e10905f4e37d21ff3cd693205752e9
veidzj <veidzjv@gmail.com>
2023-09-02 19:47:43 -0300

    refactor: move AuthMiddleware dependencies to a single file

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,7 +1,6 @@
-import { Middleware, HttpRequest, HttpResponse } from '../protocols'
+import { Middleware, LoadAccountByToken, HttpRequest, HttpResponse } from './auth-middleware-protocols'
 import { forbidden, ok, serverError } from '../helpers/http/http-helpers'
 import { AccessDeniedError } from '../errors'
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 
 export class AuthMiddleware implements Middleware {
   constructor (
