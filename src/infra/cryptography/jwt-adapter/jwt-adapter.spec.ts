e52aad582623d61d59bc9232ee8850812ce1813f
veidzj <veidzjv@gmail.com>
2023-09-04 15:57:39 -0300

    test: ensure JwtAdapter returns a decrypted value if verify succeeds

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -47,5 +47,11 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    test('Should return a decrypted value if verify succeeds', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
+    })
   })
 })
