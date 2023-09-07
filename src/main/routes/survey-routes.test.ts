c413e8b2b1f9cdca76ff0016aeeb09337dd3ec4c
veidzj <veidzjv@gmail.com>
2023-09-07 17:47:12 -0300

    refactor: move accessToken creation to a factory

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -8,6 +8,22 @@ import env from '../config/env'
 let surveysCollection: Collection
 let accountsCollection: Collection
 
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountsCollection.insertOne({
+    name: 'valid_name',
+    email: 'valid_email@mail.com',
+    password: 'valid_password',
+    role: 'admin'
+  })
+  const id = res.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountsCollection.updateOne(
+    { _id: id },
+    { $set: { accessToken } }
+  )
+  return accessToken
+}
+
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(env.mongoUrl)
@@ -43,18 +59,7 @@ describe('Survey Routes', () => {
     })
 
     test('Should return 204 if valid accessToken is provided', async () => {
-      const res = await accountsCollection.insertOne({
-        name: 'valid_name',
-        email: 'valid_email@mail.com',
-        password: 'valid_password',
-        role: 'admin'
-      })
-      const id = res.ops[0]._id
-      const accessToken = sign({ id }, env.jwtSecret)
-      await accountsCollection.updateOne(
-        { _id: id },
-        { $set: { accessToken } }
-      )
+      const accessToken = await makeAccessToken()
       await request(app)
         .post('/api/surveys')
         .set('x-access-token', accessToken)
@@ -80,45 +85,8 @@ describe('Survey Routes', () => {
         .expect(403)
     })
 
-    test('Should return 200 if valid accessToken is provided with at least one survey in the database', async () => {
-      const res = await accountsCollection.insertOne({
-        name: 'valid_name',
-        email: 'valid_email@mail.com',
-        password: 'valid_password'
-      })
-      const id = res.ops[0]._id
-      const accessToken = sign({ id }, env.jwtSecret)
-      await accountsCollection.updateOne(
-        { _id: id },
-        { $set: { accessToken } }
-      )
-      await surveysCollection.insertOne({
-        question: 'any_question',
-        answers: [
-          {
-            answer: 'any_answer',
-            image: 'any_image'
-          }],
-        date: new Date()
-      })
-      await request(app)
-        .get('/api/surveys')
-        .set('x-access-token', accessToken)
-        .expect(200)
-    })
-
-    test('Should return 204 if valid accessToken is provided with no survey in the database', async () => {
-      const res = await accountsCollection.insertOne({
-        name: 'valid_name',
-        email: 'valid_email@mail.com',
-        password: 'valid_password'
-      })
-      const id = res.ops[0]._id
-      const accessToken = sign({ id }, env.jwtSecret)
-      await accountsCollection.updateOne(
-        { _id: id },
-        { $set: { accessToken } }
-      )
+    test('Should return 204 if valid accessToken is provided', async () => {
+      const accessToken = await makeAccessToken()
       await request(app)
         .get('/api/surveys')
         .set('x-access-token', accessToken)
