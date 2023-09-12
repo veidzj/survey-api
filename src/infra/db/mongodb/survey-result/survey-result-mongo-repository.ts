03f44bafa62b8e0a00ad595f9969a23b912463c3
veidzj <veidzjv@gmail.com>
2023-09-12 18:40:28 -0300

    refactor: organize folders

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,4 +1,4 @@
-import { SaveSurveyResultRepository, SaveSurveyResultModel, SurveyResultModel } from '@/data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { SaveSurveyResultRepository, SaveSurveyResultModel, SurveyResultModel } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
