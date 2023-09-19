619fdae0fb0c3a6618987d820d3cb8dd6af8b076
veidzj <veidzjv@gmail.com>
2023-09-19 11:25:55 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Survey API',
+    description: 'Survey API using TypeScript, TDD, and Clean Architecture',
+    version: '1.0.0'
+  }
+}
