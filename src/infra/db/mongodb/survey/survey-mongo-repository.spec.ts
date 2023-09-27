4054117491a9095f6b4a7d1885a06cf3bd2af4db
veidzj <veidzjv@gmail.com>
2023-09-27 17:53:57 -0300

    test: change load surveys to add didAnswer field

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -1,13 +1,21 @@
 import { Collection } from 'mongodb'
 import { SurveyMongoRepository } from './survey-mongo-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { mockAddSurveyParams } from '@/domain/test'
+import { AccountModel } from '@/domain/models/account'
+import { mockAddAccountParams, mockAddSurveyParams } from '@/domain/test'
 import env from '@/main/config/env'
 
 let surveysCollection: Collection
+let surveysResultsCollection: Collection
+let accountsCollection: Collection
 
 const makeSut = (): SurveyMongoRepository => {
   return new SurveyMongoRepository()
+}
+
+const mockAccount = async (): Promise<AccountModel> => {
+  const res = await accountsCollection.insertOne(mockAddAccountParams())
+  return MongoHelper.map(res.ops[0])
 }
 
 describe('SurveyMongoRepository', () => {
@@ -21,7 +29,11 @@ describe('SurveyMongoRepository', () => {
 
   beforeEach(async () => {
     surveysCollection = await MongoHelper.getCollection('surveys')
+    surveysResultsCollection = await MongoHelper.getCollection('surveysResults')
+    accountsCollection = await MongoHelper.getCollection('accounts')
     await surveysCollection.deleteMany({})
+    await surveysResultsCollection.deleteMany({})
+    await accountsCollection.deleteMany({})
   })
 
   describe('add()', () => {
@@ -35,19 +47,30 @@ describe('SurveyMongoRepository', () => {
 
   describe('loadAll()', () => {
     test('Should load all surveys on success', async () => {
+      const account = await mockAccount()
       const addSurveyModels = [mockAddSurveyParams(), mockAddSurveyParams()]
-      await surveysCollection.insertMany(addSurveyModels)
+      const result = await surveysCollection.insertMany(addSurveyModels)
+      const survey = result.ops[0]
+      await surveysResultsCollection.insertOne({
+        surveyId: survey._id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
       const sut = makeSut()
-      const surveys = await sut.loadAll()
+      const surveys = await sut.loadAll(account.id)
       expect(surveys.length).toBe(2)
       expect(surveys[0].id).toBeTruthy()
       expect(surveys[0].question).toBe(addSurveyModels[0].question)
+      expect(surveys[0].didAnswer).toBe(true)
       expect(surveys[1].question).toBe(addSurveyModels[1].question)
+      expect(surveys[1].didAnswer).toBe(false)
     })
 
     test('Should load empty list if there are no surveys', async () => {
+      const account = await mockAccount()
       const sut = makeSut()
-      const surveys = await sut.loadAll()
+      const surveys = await sut.loadAll(account.id)
       expect(surveys.length).toBe(0)
     })
   })
--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,8 +1,10 @@
 import MockDate from 'mockdate'
+import faker from 'faker'
 import { LoadSurveysController } from './load-surveys-controller'
+import { HttpRequest } from './load-surveys-controller-protocols'
 import { ok, noContent, serverError } from '@/presentation/helpers/http/http-helpers'
-import { throwError } from '@/domain/test'
 import { LoadSurveysSpy } from '@/presentation/test'
+import { throwError } from '@/domain/test'
 
 type SutTypes = {
   sut: LoadSurveysController
@@ -18,6 +20,10 @@ const makeSut = (): SutTypes => {
   }
 }
 
+const mockRequest = (): HttpRequest => ({
+  accountId: faker.random.uuid()
+})
+
 describe('LoadSurveysController', () => {
   beforeAll(() => {
     MockDate.set(new Date())
@@ -27,29 +33,30 @@ describe('LoadSurveysController', () => {
     MockDate.reset()
   })
 
-  test('Should call LoadSurveys', async () => {
+  test('Should call LoadSurveys with correct value', async () => {
     const { sut, loadSurveysSpy } = makeSut()
-    await sut.handle({})
-    expect(loadSurveysSpy.callsCount).toBe(1)
+    const httpRequest = mockRequest()
+    await sut.handle(httpRequest)
+    expect(loadSurveysSpy.accountId).toBe(httpRequest.accountId)
   })
 
   test('Should return 500 if LoadSurveys throws', async () => {
     const { sut, loadSurveysSpy } = makeSut()
     jest.spyOn(loadSurveysSpy, 'load').mockImplementationOnce(throwError)
-    const httpResponse = await sut.handle({})
+    const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
   test('Should return 200 on success', async () => {
     const { sut, loadSurveysSpy } = makeSut()
-    const httpResponse = await sut.handle({})
+    const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(ok(loadSurveysSpy.surveyModels))
   })
 
   test('Should return 204 if LoadSurveys returns empty', async () => {
     const { sut, loadSurveysSpy } = makeSut()
     loadSurveysSpy.surveyModels = []
-    const httpResponse = await sut.handle({})
+    const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(noContent())
   })
 })
