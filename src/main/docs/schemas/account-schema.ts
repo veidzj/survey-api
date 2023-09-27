110ca6f9f5bffed11162b05359231464f3e90772
veidzj <veidzjv@gmail.com>
2023-09-27 15:37:44 -0300

    docs: update account schema

--- src/main/docs/schemas/account-schema.ts
+++ src/main/docs/schemas/account-schema.ts
@@ -1,9 +1,12 @@
 export const accountSchema = {
   type: 'object',
   properties: {
+    name: {
+      type: 'string'
+    },
     accessToken: {
       type: 'string'
     }
   },
-  required: ['accessToken']
+  required: ['name', 'accessToken']
 }
