55fa4a93d650a8447d630dbccc236774c9936bc5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-15 12:04:59 -0300

    fix: change loadBySurveyId to return string instead of ObjectId

--- src/infra/db/mongodb/mongo-helper.ts
+++ src/infra/db/mongodb/mongo-helper.ts
@@ -20,7 +20,7 @@ export const MongoHelper = {
 
   map: (data: any): any => {
     const { _id, ...rest } = data
-    return Object.assign({}, rest, { id: _id })
+    return Object.assign({}, rest, { id: _id.toHexString() })
   },
 
   mapCollection: (collection: any[]): any => {
